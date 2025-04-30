9696010414c6b2c0ac2e60e3f6779ddf5adf9a63
Elsa Zheng <zhengelsa150@gmail.com>
2025-04-30 09:27:22 -0700

    Navigation Bar
    Added Navigation Bar buttons to yearlyCalendarStage

--- CalendarApp.java
+++ CalendarApp.java
@@ -5,30 +5,38 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Insets;
 
 public class CalendarApp extends Application { 
-    public void start(Stage yearlyCalendarStage) 
-    { 
-        //title of page, opening screen with year
-        yearlyCalendarStage.setTitle("Welcome to the Calendar App!");
-
-        
-
-        //navigator Buttons to Yearly, Monthly, and Daily Views
-        Button yearlyView = new Button();
-        Button monthlyView = new Button();
-        Button dailyView = new Button();
-        //give the buttons text
-        yearlyView.setText("Current Year");
-        monthlyView.setText("Current Month");
-        dailyView.setText("Current Day");
-        
-        //create scene
-        //Scene yearlyScene = new Scene(200, 200);
-    }//end of start
     public static void main(String args[]) 
     { 
         // launch the application 
         launch(args); 
     }//end of main class
+    @Override
+    public void start(Stage yearlyCalendarStage) 
+    { 
+        //name of stage
+        Label yearLabel = new Label("Welcome to the Calendar App!");
+
+        //navigator Buttons to Yearly, Monthly, and Daily Views
+        Button yearlyView = new Button("Current Year");
+        Button monthlyView = new Button("Current Month");
+        Button dailyView = new Button("Current Day");
+        //create HBox for layout of navigation bar
+        HBox navigationBar = new HBox(10, yearlyView, monthlyView, dailyView);
+        navigationBar.setPadding(new Insets(10));
+        //create scene (750 pixels wide by 1334 pixels high, dimensions of my phone)
+        Scene yearlyScene = new Scene(navigationBar, 750, 1334);
+        //add yearlyScene to yearlyCalendarStage
+        yearlyCalendarStage.setScene(yearlyScene);
+        //set title for stage
+        yearlyCalendarStage.setTitle("Yearly View");
+        
+        //display stage
+        yearlyCalendarStage.show();
+    }//end of start
 }//end of calendarApp class 
